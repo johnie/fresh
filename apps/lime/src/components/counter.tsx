1b0c76656c18c463a7fd3b0bde4d522c9e05000d
Johnie Hjelm <johnie.hjelm@schibsted.com>
2026-06-16 21:07:55 +0200

    feat(lime): set up tailwind css and migrate counter component

--- apps/lime/src/components/counter.tsx
+++ apps/lime/src/components/counter.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import "./Counter.css";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
 
   return (
     <button
-      className="increment"
+      className="w-[200px] cursor-pointer rounded-[2em] border-2 border-transparent bg-[#446b9e]/10 px-[2em] py-[1em] text-[#335d92] tabular-nums outline-none focus:border-[#335d92] active:bg-[#446b9e]/20"
       onClick={() => setCount(count + 1)}
       type="button"
     >
